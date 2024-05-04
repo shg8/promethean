c662649b515f7d56e0f34e506b23c23dafa595da
shg8 <me@shg.life>
2024-05-04 00:50:58 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='promethean',
+    version='0.1.0',
+    description='Run system experiments with ease.',
+    long_description=readme,
+    author='shg8',
+    url='https://github.com/shg8/promethean',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
